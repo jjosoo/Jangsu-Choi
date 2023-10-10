e5c42e6dac91b761ac01fa79608bebad822d07ed
jjosoo <130520024+jjosoo@users.noreply.github.com>
2023-10-10 13:56:14 +0900

    1010

---
+++ script/login.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    var isYActive = true;
+
+    $(".loginBox__member-y").click(function(){
+        if (!isYActive) {
+            swapStyles(".loginBox__member-y", ".loginBox__member-n");
+            $(".loginBox__form_no").hide();
+            $(".loginBox__form").show();
+            isYActive = true;
+        }
+    });
+
+    $(".loginBox__member-n").click(function(){
+        if (isYActive) {
+            swapStyles(".loginBox__member-y", ".loginBox__member-n");
+            $(".loginBox__form").hide();
+            $(".loginBox__form_no").show();
+            isYActive = false;
+        }
+    });
+
+    function swapStyles(selector1, selector2) {
+        // 스타일 교환
+        var tempBackgroundColor = $(selector1).css("background-color");
+        var tempBorder = $(selector1).css("border");
+        var tempFontWeight = $(selector1).css("font-weight");
+        
+        $(selector1).css({
+            "background-color": $(selector2).css("background-color"),
+            "border": $(selector2).css("border"),
+            "font-weight": $(selector2).css("font-weight")
+        });
+
+        $(selector2).css({
+            "background-color": tempBackgroundColor,
+            "border": tempBorder,
+            "font-weight": tempFontWeight
+        });
+    }
+});
