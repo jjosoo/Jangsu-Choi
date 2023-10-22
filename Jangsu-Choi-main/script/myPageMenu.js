854d5f80238635efa4dc42db689282e13d404db7
jjosoo <130520024+jjosoo@users.noreply.github.com>
2023-10-23 00:16:26 +0900

    1023

--- Jangsu-Choi-main/script/myPageMenu.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $(".myPage_Menu > ul > li").mouseover(function(){
-        $(this).find(".myPage_SubMenu").stop().slideDown();
-    });
-    $(".myPage_Menu > ul > li").mouseout(function(){
-        $(this).find(".myPage_SubMenu").stop().slideUp();
-    });
-})
