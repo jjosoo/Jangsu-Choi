cb8e4687216170e4ce4136126510b88b6faf5d47
jjosoo <gkdlz7@naver.com>
2023-10-25 09:03:26 +0900

    1025

--- Jangsu-Choi-main/script/joinmain.js
+++
@@ -1,9 +0,0 @@
-// 상단 카테고리 375이하 기기 스크롤 이동
-
-var dviswidth = window.innerWidth;
-
-if( dviswidth < 375){
-
-    var hemenucon = document.querySelector('.header-menu-cont');
-    hemenucon.scrollLeft = 15;
-}
